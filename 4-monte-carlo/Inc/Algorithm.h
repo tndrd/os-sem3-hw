b037f743dd0247990a0b4df2469bc7807510719b
Vladimir Lekhterev <vovalekhterev@gmail.com>
2023-10-05 19:48:36 +0300

    Separate driver from main executable

--- 4-monte-carlo/Inc/Algorithm.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "Worker.h"
-
-double CalculateIntegralSum(const double (*foo)(double), CalculationArea limits, double ptDensity, size_t nWorkers) {
-  
-  Worker* workers = (workers)
-}
