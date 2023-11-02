fc477b32ca77fedc83e5ea513f61417e844b5d8c
Vladimir Lekhterev <vovalekhterev@gmail.com>
2023-11-02 22:34:09 +0300

    Update values in benchmark script

--- 3-ipc-comparison/Scripts/RunBenchmarks.sh
+++ 3-ipc-comparison/Scripts/RunBenchmarks.sh
@@ -1,17 +1,17 @@
 msgMax=$( cat /proc/sys/kernel/msgmax )
 
 srcFile=1gb.in
-nRepeats=5
+nRepeats=1
 
-bufSizesFifo="200 4096 1048576"
-bufSizesShm="200 4096 1048576"
-bufSizesMsg="200 4096 $msgMax"
+bufSizesFifo="16 128 4096 65536 1048576 16777216"
+bufSizesShm="16 128 4096 65536 1048576 16777216"
+bufSizesMsg="16 128 4096 $msgMax"
 
-echo "Fifo ${bufSizesFifo}:"
+echo "Fifo ${bufSizesFifo}"
 ./FifoBenchmark $srcFile $nRepeats $bufSizesFifo 2> /dev/null
 
-echo "Shm ${bufSizesShm}:"
+echo "Shm ${bufSizesShm}"
 ./ShmBenchmark $srcFile $nRepeats $bufSizesShm 2> /dev/null
 
-echo "Msg ${bufSizesMsg}:"
+echo "Msg ${bufSizesMsg}"
 ./MsgBenchmark $srcFile $nRepeats $bufSizesMsg 2> /dev/null
