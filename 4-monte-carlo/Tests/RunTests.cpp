54ec5fab3a17a6dc596f862f81e8f256bb8c6274
Vladimir Lekhterev <vovalekhterev@gmail.com>
2023-11-06 19:23:36 +0300

    Finish tests, fix errors

--- 4-monte-carlo/Tests/RunTests.cpp
+++ 4-monte-carlo/Tests/RunTests.cpp
@@ -1,6 +1,12 @@
 #include <vector>
 
+#include "TQMonitor.h"
+#include "TaskQueue.h"
+#include "WQMonitor.h"
 #include "Worker.h"
+#include "WorkerArray.h"
+#include "WorkerQueue.h"
+#include "ThreadPool.h"
 #include "gtest/gtest.h"
 
 #define CALL(foo) ASSERT_EQ(foo, STATUS_SUCCESS);
@@ -150,4 +156,299 @@ TEST(Worker, AsyncTaskChain) {
   for (int i = 0; i < NRepeats; ++i) {
     ASSERT_EQ(TestBuf[i], i * i);
   }
+}
+
+TEST(WorkerArray, Init) {
+  size_t NWorkers = 10;
+
+  WorkerArray wa;
+  CALL(WorkerArrayInit(&wa, NWorkers));
+
+  int arg = DefaultArg;
+  WorkerCallbackT callback;
+  callback.Args = &arg;
+  callback.Function = Callback;
+
+  CALL(WorkerArrayRun(&wa, callback));
+
+  for (int i = 0; i < NWorkers; ++i) {
+    Worker* worker;
+    CALL(WorkerArrayGet(&wa, i, &worker));
+
+    ASSERT_NE(worker, nullptr);
+    EXPECT_EQ(worker->ID, i);
+  }
+
+  CALL(WorkerArrayStop(&wa));
+  CALL(WorkerArrayDestroy(&wa));
+}
+
+#define FILLSIZE 625
+
+TEST(TaskQueue, FillAndFlush) {
+  TaskQueue tq;
+  CALL(TaskQueueInit(&tq));
+
+  int dummy;
+  WorkerTask task;
+
+  for (int i = 0; i < FILLSIZE; ++i) {
+    task.Args = &dummy + i;
+    CALL(TaskQueuePush(&tq, &task));
+  }
+
+  task.Args = NULL;
+  for (int i = 0; i < FILLSIZE; ++i) {
+    CALL(TaskQueuePop(&tq, &task));
+    ASSERT_EQ(task.Args, &dummy + i) << "On #" << i << std::endl;
+  }
+
+  EXPECT_EQ(TaskQueuePop(&tq, &task), STATUS_UNDERFLOW);
+
+  CALL(TaskQueueDestroy(&tq));
+}
+
+TnStatus FillStatus = STATUS_SUCCESS;
+int NFilled = 0;
+
+TnStatus FlushStatus = STATUS_SUCCESS;
+bool FlushResult = true;
+int ErrorPop = 0;
+void* ErrorVal = NULL;
+
+void* Flush(void* tqmPtr) {
+  size_t N = FILLSIZE;
+  WorkerTask task;
+  TnStatus status;
+
+  TQMonitor* tqm = (TQMonitor*)(tqmPtr);
+  task.Args = NULL;
+
+  for (int i = 0; i < N;) {
+    FlushStatus = TQMonitorGetTask(tqm, &task);
+    if (FlushStatus == STATUS_UNDERFLOW) continue;
+    if (FlushStatus != STATUS_SUCCESS) {
+      ErrorPop = i;
+      TQMonitorSignalError(tqm);
+      break;
+    }
+    
+    if (task.Args != tqm + i) {
+      FlushResult = false;
+      ErrorPop = i;
+      ErrorVal = task.Args;
+      TQMonitorSignalError(tqm);
+      break;
+    }
+
+    i++;
+  }
+
+  return NULL;
+}
+
+TEST(TQMonitor, FillFlushWait) {
+  FlushStatus = STATUS_SUCCESS;
+  FlushResult = true;
+  ErrorPop = 0;
+  ErrorVal = 0;
+
+  TQMonitor tqm;
+  CALL(TQMonitorInit(&tqm));
+
+  pthread_t thread;
+  ASSERT_EQ(pthread_create(&thread, NULL, Flush, &tqm), 0);
+
+  WorkerTask task;
+  for (int i = 0; i < FILLSIZE; ++i) {
+    task.Args = &tqm + i;
+    CALL(TQMonitorAddTask(&tqm, &task));
+  }
+
+  TQMonitorWaitEmpty(&tqm);
+
+  if (FlushStatus != STATUS_SUCCESS) {
+    ASSERT_EQ(FlushStatus, STATUS_SUCCESS)
+        << "On pop: " << ErrorPop << std::endl;
+  }
+
+  if (!FlushResult) {
+    ASSERT_EQ(FlushResult, true)
+        << "On pop: " << ErrorPop << " with value: " << ErrorVal << " expected: " << &tqm + ErrorPop << std::endl;
+  }
+
+  WorkerTask dummy;
+  EXPECT_EQ(TQMonitorGetTask(&tqm, &dummy), STATUS_UNDERFLOW);
+
+  CALL(TQMonitorDestroy(&tqm));
+
+  pthread_join(thread, NULL);
+}
+
+TEST(WorkerQueue, FillAndFlush) {
+  size_t NWorkers = 10;
+  WorkerQueue wq;
+  WorkerID id;
+
+  WorkerQueueInit(&wq, NWorkers);
+
+  ASSERT_EQ(WorkerQueuePop(&wq, &id), STATUS_UNDERFLOW);
+
+  for (int i = 0; i < NWorkers; ++i) {
+    id = i;
+    CALL(WorkerQueuePush(&wq, &id));
+  }
+
+  ASSERT_EQ(WorkerQueuePush(&wq, &id), STATUS_OVERFLOW);
+
+  id = 42;
+
+  for (int i = 0; i < NWorkers; ++i) {
+    CALL(WorkerQueuePop(&wq, &id));
+    ASSERT_EQ(id, i);
+  }
+
+  ASSERT_EQ(WorkerQueuePop(&wq, &id), STATUS_UNDERFLOW);
+
+  CALL(WorkerQueueDestroy(&wq));
+}
+
+void* FillWQ(void* wqPtr) {
+  WQMonitor* wq = (WQMonitor*)wqPtr;
+
+  for (int i = 0; i < FILLSIZE; ++i) {
+    WorkerID id = i;
+
+    FillStatus = WQMonitorAddWorker(wq, &id);
+    if (FillStatus != STATUS_SUCCESS) {
+      NFilled = i + 1;
+      WQMonitorSignalError(wq);
+      break;
+    }
+  }
+
+  return NULL;
+}
+
+TEST(WQMonitor, FillWait) {
+  FillStatus = STATUS_SUCCESS;
+  NFilled = 0;
+
+  WQMonitor wq;
+  WorkerID id;
+  CALL(WQMonitorInit(&wq, FILLSIZE));
+
+  ASSERT_EQ(WQMonitorGetWorker(&wq, &id), STATUS_UNDERFLOW);
+
+  pthread_t thread;
+  int ret = pthread_create(&thread, NULL, FillWQ, &wq);
+  ASSERT_EQ(ret, 0);
+
+  WQMonitorWaitFull(&wq);
+
+  ASSERT_EQ(FillStatus, STATUS_SUCCESS) << "Filled totally: " << NFilled << std::endl;
+  ASSERT_EQ(WQMonitorAddWorker(&wq, &id), STATUS_OVERFLOW);
+
+  for (int i = 0; i < FILLSIZE; ++i) {
+    CALL(WQMonitorGetWorker(&wq, &id));
+    EXPECT_EQ(id, i);
+  }
+
+  ASSERT_EQ(WQMonitorGetWorker(&wq, &id), STATUS_UNDERFLOW);
+
+  CALL(WQMonitorDestroy(&wq));
+  pthread_join(thread, NULL);
+}
+
+TEST(ThreadPool, Init) {
+  size_t NWorkers = 10;
+  ThreadPool tp;
+
+  CALL(ThreadPoolInit(&tp, NWorkers));
+  CALL(ThreadPoolRun(&tp));
+  CALL(ThreadPoolStop(&tp));
+  CALL(ThreadPoolDestroy(&tp));
+}
+
+struct TaskData {
+  int Arg;
+  int Res;
+  TnStatus Status; 
+};
+
+template<size_t NWorkers, size_t NTasks>
+void TestThreadPool() {
+  WorkerTask Tasks[NTasks];
+  TaskData TasksImpl[NTasks];
+
+  for (int i = 0; i < NTasks; ++i) {
+    TasksImpl[i].Arg = i;
+    TasksImpl[i].Res = -1;
+    TasksImpl[i].Status = STATUS_UNDEFINED;
+
+    Tasks[i].Args = &TasksImpl[i].Arg;
+    Tasks[i].Result = &TasksImpl[i].Res;
+    Tasks[i].Status = &TasksImpl[i].Status;
+    Tasks[i].Function = Pow;
+  }
+
+  ThreadPool tp;
+  CALL(ThreadPoolInit(&tp, NWorkers));
+  CALL(ThreadPoolRun(&tp));
+
+  for (int i = 0; i < NTasks; ++i) {
+    CALL(ThreadPoolAddTask(&tp, Tasks[i]));
+  }
+
+  CALL(ThreadPoolWaitAll(&tp));
+
+  for (int i = 0; i < NTasks; ++i) {
+    ASSERT_EQ(TasksImpl[i].Status, STATUS_SUCCESS);
+    ASSERT_EQ(TasksImpl[i].Res, i * i);
+  }
+
+  CALL(ThreadPoolStop(&tp));
+  CALL(ThreadPoolDestroy(&tp));
+}
+
+TEST(ThreadPool, OneWorkerOneTask) {
+  static constexpr size_t NTasks = 1;
+  static constexpr size_t NWorkers = 1;
+
+  TestThreadPool<NWorkers, NTasks>();
+}
+
+TEST(ThreadPool, OneWorkerManyTasks) {
+  static constexpr size_t NTasks = 10000;
+  static constexpr size_t NWorkers = 1;
+
+  TestThreadPool<NWorkers, NTasks>();
+}
+
+TEST(ThreadPool, ManyWorkersOneTask) {
+  static constexpr size_t NTasks = 1;
+  static constexpr size_t NWorkers = 100;
+
+  TestThreadPool<NWorkers, NTasks>();
+}
+
+TEST(ThreadPool, FewWorkersManyTasks) {
+  static constexpr size_t NTasks = 10000;
+  static constexpr size_t NWorkers = 5;
+
+  TestThreadPool<NWorkers, NTasks>();
+}
+
+TEST(ThreadPool, ManyWorkersFewTasks) {
+  static constexpr size_t NTasks = 10;
+  static constexpr size_t NWorkers = 100;
+
+  TestThreadPool<NWorkers, NTasks>();
+}
+
+TEST(ThreadPool, EqualWorkersTasks) {
+  static constexpr size_t NTasks = 100;
+  static constexpr size_t NWorkers = 100;
+
+  TestThreadPool<NWorkers, NTasks>();
 }
